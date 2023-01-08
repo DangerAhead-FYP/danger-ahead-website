51f58569803d8c4f318fbfff4f3f876039f5837b
Abhilashax <abhinanda361@gmail.com>
2023-01-08 18:37:23 +0530

    Added Navbar, Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from "../src/components/Home"
+import Accidents from "../src/components/Accidents"
+import Analysis from "../src/components/Analysis"
+import Markaccidents from "../src/components/Markaccidents"
 
 function App() {
   return (
-    <h1 className='text-danger'>Hiii</h1>
+    <Router>
+      <div>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} active="home"/>
+            <Route exact path="/accidents" element={<Accidents />} active="accidents"/>
+            <Route exact path="/analysis" element={<Analysis />} active="analysis"/>
+            <Route exact path="/markaccidents" element={<Markaccidents/>} active="markaccidents"/>               
+          </Routes>
+        </div>
+
+      </div>
+    </Router>
   );
 }
 
