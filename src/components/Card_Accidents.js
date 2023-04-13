59273547e39b563a6abb7c1d569b47abea0d96ed
alokkumax <alokkumax1@gmail.com>
2023-04-13 22:27:20 +0530

    added markers

--- src/components/Card_Accidents.js
+++ src/components/Card_Accidents.js
@@ -8,30 +8,30 @@ import { AiOutlineClockCircle } from 'react-icons/ai';
 export default function Card_Accidents() {
   return (
     <div className="card">
-                  <img
-                    className="card-img-top"
-                    src={accident_img}
-                    alt="Card image cap"
-                  />
-                <div className="card-body">
-                    <div className="row">
-                      <div className="col-7">
-                        <h6 className='f-20 inter mb-0'>
-                        Car Accident
-                        </h6>
-                        <p className='fw-semibold f-12 light-grey'><span className='orange'><MdLocationOn/></span>
-                        Venus More, Siliguri</p>
-                      </div>
-                      <div className="col-5">
-                        <button className='button red-btn-outline'>Details</button>
-                      </div>
-                    </div>
-                    <p className='habibi f-12 grey lh-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                    <p className='habibi f-12 light-grey lh-sm mb-0'>Number of Injured : 3 </p>
-                    <p className='habibi f-12 light-grey lh-sm'>Number of Deaths : 1 </p>
-                    <p className='fw-semibold f-12 dark-grey mb-0'><span><SlCalender/></span>
-                    &nbsp; 24-05-22&nbsp; <span><AiOutlineClockCircle/></span>&nbsp; 11:06 PM</p>
-                </div>
-              </div>
+      <img
+        className="card-img-top"
+        src={accident_img}
+        alt="Card image cap"
+      />
+      <div className="card-body">
+        <div className="row">
+          <div className="col-7">
+            <h6 className='f-20 inter mb-0'>
+              Car Accident
+            </h6>
+            <p className='fw-semibold f-12 light-grey'><span className='orange'><MdLocationOn /></span>
+              Venus More, Siliguri</p>
+          </div>
+          <div className="col-5">
+            <button className='button red-btn-outline'>Details</button>
+          </div>
+        </div>
+        <p className='habibi f-12 grey lh-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        <p className='habibi f-12 light-grey lh-sm mb-0'>Number of Injured : 3 </p>
+        <p className='habibi f-12 light-grey lh-sm'>Number of Deaths : 1 </p>
+        <p className='fw-semibold f-12 dark-grey mb-0'><span><SlCalender /></span>
+          &nbsp; 24-05-22&nbsp; <span><AiOutlineClockCircle /></span>&nbsp; 11:06 PM</p>
+      </div>
+    </div>
   )
 }
--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,15 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { MarkerF, InfoWindowF, InfoBoxF } from "@react-google-maps/api";
+import customMarker from "../images/acci.png";
+import acci from "../data.json"
+import mapStyles from "../stylesheets/mapStyles"
+
+//CARD
+import "../stylesheets/Card_Accidents.css"
+import accident_img from "../images/accident-img.png"
+import { MdLocationOn } from 'react-icons/md';
+import { SlCalender } from 'react-icons/sl';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+
 const containerStyle = {
     width: "100%",
-    height: "720px",
+    height: "100vh",
 };
 
 const center = {
-    lat: 10.894421705347245,
-    lng: 76.99704007070052
+    lat: 26.688315262877015,
+    lng: 88.4347097728035,
 };
+
 const Maps = () => {
+    const [selectedAcci, setSelectedAcci] = useState(null);
+
     const { isLoaded } = useJsApiLoader({
         id: "google-map-script",
         googleMapsApiKey: "AIzaSyAObG5o48uk0MFpkCkpebOzF1vHt07M-TI",
@@ -28,7 +43,7 @@ const Maps = () => {
     }, []);
 
     return (
-        <div >
+        <div>
             {isLoaded ? (
                 <GoogleMap
                     mapContainerStyle={containerStyle}
@@ -36,9 +51,62 @@ const Maps = () => {
                     zoom={18}
                     onLoad={onLoad}
                     onUnmount={onUnmount}
+                // options={{ styles: mapStyles,  }}
                 >
-                    {/* Child components, such as markers, info windows, etc. */}
-                    <></>
+                    {
+                        acci.data.map((acci) => {
+                            return (
+                                <div key={acci.id}>
+                                    <MarkerF
+                                        position={{ lat: acci.x, lng: acci.y }}
+                                        options={{ icon: customMarker }}
+                                        onClick={() => {
+                                            setSelectedAcci(acci);
+                                        }}
+                                    />
+                                </div>
+                            );
+                        })
+                    }
+                    {selectedAcci && (
+                        <InfoWindowF
+
+                            position={{
+                                lat: selectedAcci.x,
+                                lng: selectedAcci.y
+                            }}
+                            onCloseClick={() => {
+                                setSelectedAcci(null)
+                            }}
+                        >
+                            <div className="card">
+                                <img
+                                    className="card-img-top"
+                                    src={accident_img}
+                                    alt="Card image cap"
+                                />
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col-7">
+                                            <h6 className='f-20 inter mb-0'>
+                                                {selectedAcci.title}
+                                            </h6>
+                                            <p className='fw-semibold f-12 light-grey'><span className='orange'><MdLocationOn /></span>
+                                                Venus More, Siliguri</p>
+                                        </div>
+                                        <div className="col-5">
+                                            <button className='button red-btn-outline'>Details</button>
+                                        </div>
+                                    </div>
+                                    <p className='habibi f-12 grey lh-sm'>{selectedAcci.desc}</p>
+                                    <p className='habibi f-12 light-grey lh-sm mb-0'>Number of Injured : {selectedAcci.NumberOfInjured}</p>
+                                    <p className='habibi f-12 light-grey lh-sm'>Number of Deaths : 1 </p>
+                                    <p className='fw-semibold f-12 dark-grey mb-0'><span><SlCalender /></span>
+                                        &nbsp; {selectedAcci.eventDate}&nbsp; <span><AiOutlineClockCircle /></span>&nbsp;{selectedAcci.eventTime}</p>
+                                </div>
+                            </div>
+                        </InfoWindowF>
+                    )}
                 </GoogleMap>
             ) : (
                 <></>
