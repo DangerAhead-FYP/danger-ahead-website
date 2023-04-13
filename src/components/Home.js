f686f12af81e72f0c8c312230cd148aa76da9c31
Abhilasha <abhinanda361@gmail.com>
2023-04-13 19:12:35 +0530

    Added Road Accidents Status and fixed respomsive issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,14 @@ import Navbar from "../components/Navbar"
 import Card_Warnings from './Card_Warnings'
 // import Card from './Card_Accidents'
 import Hero from './Hero'
+import RoadAccidentsStatus from './RoadAccidentsStatus'
 
 export default function Home(props) {
   return (
     <div>
         <Navbar active={props.active}/>
         <Hero/>
-        <Card_Warnings/>
+        <RoadAccidentsStatus/>
     </div>
   )
 }
