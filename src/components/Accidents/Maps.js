b1f6a761e90899bbd3b87af360c49089514b2ced
alokkumax <alokkumax1@gmail.com>
2023-04-15 13:30:04 +0530

    added fetch user curr Location

--- src/components/Accidents/Maps.js
+++ src/components/Accidents/Maps.js
@@ -4,13 +4,9 @@ import { MarkerF, InfoWindowF, InfoBoxF } from "@react-google-maps/api";
 import customMarker from "../../images/acci.png";
 import acci from "../../data.json"
 import mapStyles from "../../stylesheets/Accidents/mapStyles"
-
+import curr from "../../images/curr.png"
+import "../../stylesheets/Accidents/maps.css"
 //CARD
-import "../../stylesheets/Accidents/Card_Accidents.css"
-import accident_img from "../../images/accident-img.png"
-import { MdLocationOn } from 'react-icons/md';
-import { SlCalender } from 'react-icons/sl';
-import { AiOutlineClockCircle } from 'react-icons/ai';
 import Card_Accidents from "./Card_Accidents";
 
 const containerStyle = {
@@ -22,16 +18,45 @@ const center = {
     lat: 26.769290864950857,
     lng: 88.37616388253649,
 };
+// const currL ={
 
+// }
+function Locate({ const: center }) {
+    return (
+        <button
+            className="curr"
+            onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        console.log(position);
+                        const currL = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        }
+                        console.log(currL)
+                        currL = center;
+                        // setCurr(currL)
+                    },
+                    () => null
+                )
+            }}
+        >
+            <img className="currI" src={curr} sizes="20px" />
+        </button>
+    )
+}
+const libraries = ["places"]
 const Maps = () => {
     const [selectedAcci, setSelectedAcci] = useState(null);
 
     const { isLoaded } = useJsApiLoader({
         id: "google-map-script",
         googleMapsApiKey: "AIzaSyAObG5o48uk0MFpkCkpebOzF1vHt07M-TI",
+        libraries,
     });
 
     const [map, setMap] = useState(null);
+    const [currL, setCurr] = useState(center);
 
     const onLoad = React.useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds(center);
@@ -52,6 +77,7 @@ const Maps = () => {
                     zoom={18}
                     onLoad={onLoad}
                     onUnmount={onUnmount}
+
                 // options={{ styles: mapStyles,  }}
                 >
                     {
@@ -79,17 +105,21 @@ const Maps = () => {
                             onCloseClick={() => {
                                 setSelectedAcci(null)
                             }}
-                            // options={{}}
+                        // options={{}}
                         >
 
-                        <Card_Accidents title={selectedAcci.title} desc={selectedAcci.desc} no_injured={selectedAcci.NumberOfInjured} no_deaths={selectedAcci.NumberOfDeaths} date={selectedAcci.eventDate} time={selectedAcci.eventTime}/>    
+                            <Card_Accidents title={selectedAcci.title} desc={selectedAcci.desc} no_injured={selectedAcci.NumberOfInjured} no_deaths={selectedAcci.NumberOfDeaths} date={selectedAcci.eventDate} time={selectedAcci.eventTime} />
 
                         </InfoWindowF>
                     )}
+
                 </GoogleMap>
+
             ) : (
                 <></>
             )}
+            <Locate />
+
         </div>
     );
 };
